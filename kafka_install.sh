b87e2cbab3c83d392a9853fc6091b0d945721cc9
PLgitdev <50424549+PLgitdev@users.noreply.github.com>
2021-09-11 11:28:53 -0500

    minimal

--- kafka_install.sh
+++ kafka_install.sh
@@ -10,46 +10,6 @@ VER='2.6.2'
 # Version prefix
 PVER='2.13'
 DLURL="https://downloads.apache.org/kafka/2.6.2/kafka_2.13-2.6.2.tgz"
-PROPS='home/kafka/kafka/config/server.properties'
-# Log destination
-LOGDEST='home/kafka/kafka/logs'
-# Zookeeper service path
-ZKPATH='/etc/systemd/system/zookeeper.service'
-KPATH='/etc/systemd/system/kafka.service'
-# Unit file def for Zookeeper
-ZKUDEF=$(cat << EOF
-[Unit]\n
-Requires=network.target remote-fs.target\n
-After=network.target remote-fs.target\n
-\n
-[Service]\n
-Type=simple\n
-User=kafka\n
-ExecStart=/home/kafka/kafka/bin/zookeeper-server-start.sh /home/kafka/kafka/config/zookeeper.properties\n
-ExecStop=/home/kafka/kafka/bin/zookeeper-server-stop.sh\n
-Restart=on-abnormal\n
-\n
-[Install]\n
-WantedBy=multi-user.target\n
-EOF
-)
-# Unit file def for Kafka
-KUDEF=$(cat << EOF
-[Unit]\n
-Requires=zookeeper.service\n
-After=zookeeper.service\n
-\n
-[Service]\n
-Type=simple\n
-User=kafka\n
-ExecStart=/bin/sh -c '/home/kafka/kafka/bin/kafka-server-start.sh /home/kafka/kafka/config/server.properties > /home/kafka/kafka/kafka.log 2>&1'\n
-ExecStop=/home/kafka/kafka/bin/kafka-server-stop.sh\n
-Restart=on-abnormal\n
-\n
-[Install]\n
-WantedBy=multi-user.target
-EOF
-)
 
 
 echo "Please enter sudo password"
@@ -81,59 +41,4 @@ else
     echo $SUDOPASS | sudo -S tar -xvzf $DLDIR/$TARFILE -C $KDIR --strip 1
 fi
 
-# configure Kafka
-echo "Would you like to configure $TARFILE?\n(This will allow you to delte topics and setup logs)"
-read CONFIG
-if [ "$CONFIG" = "yes" ];
-then
-    echo $SUDOPASS | sudo -S echo 'delete.topic.enable = true' >> echo $SUDOPASS | sudo -S tee -a $PROPS
-    if grep -q 'delete.topic.enable=true' $PROPS;
-    then
-        echo 'Delete topic has been enabled'
-    else
-        echo 'ERROR'
-    fi
-    grep 'delete.topic.enable*' $PROPS
-    sed -i "s/log.dirs=*/log.dirs=$LOGSDEST" $PROPS
-    if grep -q "log.dirs=$LOGDEST" $PROPS;
-    then
-        echo "$LOGDEST set"
-    else
-        echo 'ERROR'
-    fi
-    grep "log.dirs=$LOGDEST" $PROPS
-else
-    echo "You have chosen not to configure $TARFILE you can always change it manually."
-fi
-
-# Create Systemd Unit Files and Start the Kafka Server
-echo "Would you like to create systemd Unit files?"
-read UNITFILES
-if [ "$UNITFILES" = "yes" ];
-then
-    echo $SUDOPASS | sudo -S touch $ZKPATH
-    echo $ZKUDEF > $ZKPATH
-    echo $SUDOPASS | sudo -S touch $KPATH
-    echo $KUDEF > $KUDEF
-    cat $ZKPATH
-    cat $KPATH
-else
-    echo 'You have chosen not to create the Unit files you can alawys change it manually.'
-fi
-echo 'would you like to start kafka server?'
-read START
-if $START = "yes"
-then
-    echo $SUDOPASS | sudo -S systemctl start kafka
-    if sudo grep -q Active: active | systemctl status kafka
-    then
-        echo "Kafka is running successfully!"
-    else
-        echo "ERROR"
-    fi
-    echo $SUDOPASS | sudo -S systemctl status kafka
-else
-    echo 'You have chosen not to start kafka you can always start it manually.'
-fi
-echo "Goodbye"
 exit 1
